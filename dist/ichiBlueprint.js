2e33862382e0ed23672b759b2dc43f0f43307fc1
jawaccomplishedclass <>
2024-02-13 17:55:42 +0900

    add test wallet addresses

--- dist/ichiBlueprint.js
+++ dist/ichiBlueprint.js
@@ -8,7 +8,11 @@ class IchiBlueprint {
         this.context = context;
     }
     getTestWalletAddresses() {
-        return [''];
+        return [
+            '0x569a4d03a9b775a64e1e6b2928d699b345d8bdca',
+            '0x0e064ea0a357d9f8bc5b74a3c57bc88b4c48a53d',
+            '0x73ca04a777fd8d1681a253e8ab958b8fef1c0a24',
+        ];
     }
     syncMetadata(_metadataStore, _lastSyncAt) {
         return Promise.resolve(0);
--- src/ichiBlueprint.ts
+++ src/ichiBlueprint.ts
@@ -22,7 +22,11 @@ export abstract class IchiBlueprint implements Blueprint {
   constructor(protected context: BlueprintContext) {}
 
   getTestWalletAddresses(): string[] {
-    return [''];
+    return [
+      '0x569a4d03a9b775a64e1e6b2928d699b345d8bdca', // Partial exit
+      '0x0e064ea0a357d9f8bc5b74a3c57bc88b4c48a53d', // Full exit
+      '0x73ca04a777fd8d1681a253e8ab958b8fef1c0a24', // Re-entering after full exit
+    ];
   }
 
   syncMetadata(_metadataStore: MetadataStore, _lastSyncAt: number): Promise<number> {
